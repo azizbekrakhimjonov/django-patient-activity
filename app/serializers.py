33c513a29508d0dea6243936223d0bf75cc5fe0d
Azizbek Rahimjonov <azizbekrahimjonov571@gmail.com>
2024-10-14 18:34:34 +0500

    Update serializers.py

--- app/serializers.py
+++ app/serializers.py
@@ -1,3 +1,27 @@
+# from rest_framework import serializers
+# from .models import Patient, PatientActivity
+# from django.contrib.auth.models import User
+#
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta(object):
+#         model = User
+#         fields = ['id', 'username', 'password', 'email']
+#
+# class PatientSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Patient
+#         fields = '__all__'
+#
+# class PatientActivitySerializer(serializers.ModelSerializer):
+#     duration = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = PatientActivity
+#         fields = '__all__'
+#
+#     def get_duration(self, obj):
+#         return obj.duration()
+
 from rest_framework import serializers
 from .models import Patient, PatientActivity
 from django.contrib.auth.models import User
@@ -14,10 +38,14 @@ class PatientSerializer(serializers.ModelSerializer):
 
 class PatientActivitySerializer(serializers.ModelSerializer):
     duration = serializers.SerializerMethodField()
+    mobile_number = serializers.SerializerMethodField()
 
     class Meta:
         model = PatientActivity
-        fields = '__all__'
+        fields = ['id', 'date', 'start_time', 'end_time', 'duration', 'mobile_number']
 
     def get_duration(self, obj):
         return obj.duration()
+
+    def get_mobile_number(self, obj):
+        return obj.patient.mobile_number
