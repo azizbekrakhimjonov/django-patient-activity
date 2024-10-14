771a0f5a7cb3ee99f0b0af719e66ae08f7fe559c
Azizbek Rahimjonov <azizbekrahimjonov571@gmail.com>
2024-10-14 18:34:59 +0500

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -5,9 +5,20 @@ from .models import Patient, PatientActivity
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'age', 'mobile_number')
 
+# @admin.register(PatientActivity)
+# class PatientActivityAdmin(admin.ModelAdmin):
+#     list_display = ('mobile_number', 'patient', 'date', 'start_time', 'end_time', 'duration')
+
 @admin.register(PatientActivity)
 class PatientActivityAdmin(admin.ModelAdmin):
-    list_display = ('id', 'patient', 'date', 'start_time', 'end_time', 'duration')
+    list_display = ('patient_mobile_number', 'patient', 'date', 'start_time', 'end_time', 'duration')
+
+    def patient_mobile_number(self, obj):
+        return obj.patient.mobile_number
+
+    patient_mobile_number.short_description = 'Mobile Number'
+
+
 # from django.contrib import admin
 # from .models import Patient, PatientActivity
 #
